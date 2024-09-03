50cffff16357fa2b015df78def42391e10042c4a
Aamir <araja@ycds.org>
2024-09-03 18:52:39 -0400

    aamir AUtonomoun, probably has like 30 errors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AA_AutonCommands/Aamir_Auton.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode.AA_AutonCommands;
+
+import com.qualcomm.hardware.dfrobot.HuskyLens;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.AutonCommands.MoveForDistance;
+import org.firstinspires.ftc.teamcode.AutonCommands.Reposition;
+import org.firstinspires.ftc.teamcode.AutonCommands.RepositionAndSlide;
+import org.firstinspires.ftc.teamcode.AutonCommands.SetClaws;
+import org.firstinspires.ftc.teamcode.AutonCommands.SetWrist;
+import org.firstinspires.ftc.teamcode.AutonCommands.StrafeForDistance;
+import org.firstinspires.ftc.teamcode.AutonCommands.ToBlue;
+import org.firstinspires.ftc.teamcode.AutonCommands.ToBlueStrafe;
+import org.firstinspires.ftc.teamcode.AutonCommands.TurnAndSlide;
+import org.firstinspires.ftc.teamcode.AutonCommands.TurnToHeading;
+import org.firstinspires.ftc.teamcode.AutonCommands.WaitForTime;
+import org.firstinspires.ftc.teamcode.Hardware.Command;
+import org.firstinspires.ftc.teamcode.AutonCommands.SlideToPosition;
+import org.firstinspires.ftc.teamcode.Hardware.HackinHoundsHardware;
+
+import java.util.ArrayList;
+import java.util.List;
+public class Aamir_Auton extends LinearOpMode{
+    private HackinHoundsHardware robot = new HackinHoundsHardware();
+
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private boolean
+
+    @Override
+    public void runOpMode()  {
+
+    }
+}
